d13b0adcdb8d205dd182bd8bb5ff075c92d246d4
Ugo Sangiorgi <ugo@ugosan.org>
2014-07-07 16:54:32 +0200

    Fix the image writing (PNG instead of webp)

--- c/write.php
+++ c/write.php
@@ -1,7 +1,8 @@
 <?php
 $imageInfo = $_POST['imageData'];
-
+imagefilter($imageInfo, IMG_FILTER_PIXELATE, 5, true);
 $image = fopen($imageInfo, 'w');
+
 file_put_contents("../snapshots/snapshot.png", $image);
 fclose($image);
 
